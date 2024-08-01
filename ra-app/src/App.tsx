3723ad50e669775ba79c2592322119e9d1aeab36
andro-jr <prabin20panta@gmail.com>
2024-08-01 12:21:55 +0545

    feat: poster edit

--- ra-app/src/App.tsx
+++ ra-app/src/App.tsx
@@ -2,12 +2,13 @@ import "./App.css";
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { PosterList } from "./components/PosterList";
+import { PosterEdit } from "./components/PosterEdit";
 
 const dataProvider = jsonServerProvider("http://localhost:3000");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posters" list={PosterList} />
+    <Resource name="posters" list={PosterList} edit={PosterEdit} />
   </Admin>
 );
 

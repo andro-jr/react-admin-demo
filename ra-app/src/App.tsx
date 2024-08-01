1605d4f4512b83c3fa80890b8f1b462c64ba3d54
andro-jr <prabin20panta@gmail.com>
2024-08-01 12:17:33 +0545

    add: poster list

--- ra-app/src/App.tsx
+++ ra-app/src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
-import { Admin, ListGuesser, Resource } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
+import { PosterList } from "./components/PosterList";
 
 const dataProvider = jsonServerProvider("http://localhost:3000");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posters" list={ListGuesser} />
+    <Resource name="posters" list={PosterList} />
   </Admin>
 );
 
